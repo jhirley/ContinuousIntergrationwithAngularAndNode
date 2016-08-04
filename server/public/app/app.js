e1003ead75531dc025a36ab4aabbd0e33aedb18c
Jhirley <jhirley.fonte@gmail.com>
2016-08-03 20:54:28 -0400

    Jobs Posting Form

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -5,5 +5,12 @@ var app = angular.module('app',['ngResource']);
 angular.module('app').controller('testCtrl', function($scope, $resource,jobs){
 	$scope.jobs = $resource('/api/jobs').query();
 	$scope.test = 'working';
-	jobs.save({title: 'test title', description: 'test description'});
+	// 
+
+	$scope.submit = function() {
+		var job = {title: $scope.title, description: $scope.description};
+		jobs.save(job);
+		$scope.jobs.push(job);
+
+	}
 });
